d6be5e9487f25e4cd28cdbb4c7e105fe63c3403b
Fabien Duranton <f.m.duranton@gmail.com>
2012-04-02 12:01:29 +0200

    First impl

---
+++ src/DTDClass/ElementAttBase.h
@@ -0,0 +1,49 @@
+/*************************************************************************
+                           ELEMENT_ATT_BASE  -  description
+                             -------------------
+    début                : 02/04/2012
+    copyright            : (C) 2012 par fduranton, dbrian, recastrodiaz
+    e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Interface du module <ElementAttBase> (fichier ElementAttBase.h) ---------
+#if ! defined ( ELEMENT_ATT_BASE_H )
+#define ELEMENT_ATT_BASE_H
+
+//------------------------------------------------------------------------
+// Rôle du module <BALISE>
+//
+//
+//------------------------------------------------------------------------
+
+/////////////////////////////////////////////////////////////////  INCLUDE
+//--------------------------------------------------- Interfaces utilisées
+#include <string>
+#include <stdlib>
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//////////////////////////////////////////////////////////////////  PUBLIC
+//---------------------------------------------------- Fonctions publiques
+// type Nom ( liste de paramètres );
+// Mode d'emploi :
+//
+// Contrat :
+//
+
+class ElementAttBase
+{
+	public:
+		virtual ElementAttBase() = 0;
+		virtual ~ElementAttBase();
+
+		typedef enum Cardinalite ( ETOILE, PLUS );
+		typedef enum ContentSpedc ( EMPTY, ANY );
+
+};
+
+#endif // ELEMENT_ATT_BASE_H
+
+
+
